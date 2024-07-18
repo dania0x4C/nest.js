471d5f9908dfe54a349b87bc02f00b513c1ff0dd
dania0x4C <ljm31881004@gmail.com>
2024-07-18 23:45:55 +0900

    middleware

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,39 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CustomersController } from './controller/customers/customers.controller';
 import { CustomersService } from './services/customers/customers.service';
+import { ValidateCustomerMiddleWare } from './middlewares/validate-customer.middleware';
+import { ValidateCustomerAccountMiddleware } from './middlewares/validate-customer-account.middleware';
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService],
 })
-export class CustomersModule {}
+export class CustomersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateCustomerMiddleWare, ValidateCustomerAccountMiddleware)
+      .exclude(
+        {
+          path: 'api/customers/create',
+          method: RequestMethod.POST,
+        },
+        {
+          path: 'api/customers',
+          method: RequestMethod.GET,
+        },
+      )
+      .forRoutes(
+        CustomersController,
+        // {
+        //   path: 'customers/search/:id',
+        //   method: RequestMethod.GET,
+        // },
+        // {},여러개 전달 가능
+      );
+  }
+}
---
+++ src/customers/middlewares/validate-customer-account.middleware.ts
@@ -0,0 +1,16 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class ValidateCustomerAccountMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const { validAccount } = req.headers;
+    console.log(validAccount);
+    console.log('ValidateCustomerAccount');
+    if (validAccount) {
+      next();
+    } else {
+      res.status(401).send({ error: 'Account is invalid' });
+    }
+  }
+}
